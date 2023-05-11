6d924234719eec72168b9e98541a969be50f465c
Shawn Andrews <andrews.shawn@gmail.com>
2023-05-11 13:15:55 -0500

    fix lsblk

---
+++ disk_scan.py
@@ -0,0 +1,11 @@
+import socket
+from subprocess import check_output
+from itertools import zip_longest
+
+class DiskScan():
+
+    def localdisk(hostname):
+        kname - check_output(("lsblk", "-io", "KNAME"))
+        print(kname)
+
+DiskScan.localdisk(hostname = socket.gethostname())
